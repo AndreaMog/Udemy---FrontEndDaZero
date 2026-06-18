0732adba983672a1f267572ad456bae9b0d75077
Andrea_Mog <andreajohnbrian@gmail.com>
2026-06-18 14:50:08 +0200

    Aggiunta la funzionalità del serve.js

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -3,8 +3,15 @@ const { watchJs } = require("./jsTasks");
 const series = gulp.series;
 const viewTasks = require("./viewTasks");
 const jsTasks = require("./jsTasks");
+const serveTasks = require("./serveTasks");
 
-const dev = series(viewTasks.compileIndex, jsTasks.copyJs, jsTasks.watchJs, viewTasks.watchIndex /*, serve, watchHtml*/);
+const dev = series(
+        viewTasks.compileIndex, 
+        jsTasks.copyJs, 
+        jsTasks.watchJs, 
+        viewTasks.watchIndex, 
+        serveTasks.serve
+    );
 
 module.exports = {
     dev: dev
